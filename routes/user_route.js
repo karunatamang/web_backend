182443bdd61622c31b25d53a1819fe1ad924662f
karunatamang <korunatamang12@gmail.com>
2020-01-28 23:06:37 -0800

    user route

--- routes/user_route.js
+++ routes/user_route.js
@@ -4,14 +4,12 @@ const Users = require('../models/User')
 const auth = require('../middleware/auth');
 const UserController = require('../controller/user_controller')
 
+router.post("/register",UserController.adduser);
+router.post("/login", UserController.login);
 
-
-router.post("/register",UserController.adduser)
-router.post("/login", UserController.login)
-
-router.get("/userdetail/:id", UserController.userdetail)
-router.put("/update/:id", UserController.updateUser)
-router.delete("/delete/:id", UserController.deleteUser)
-router.post("/logout", UserController.logout)
+router.get("/userdetail/:id", UserController.userdetail);
+router.put("/update/:id", UserController.updateUser);
+router.delete("/delete/:id", UserController.deleteUser);
+router.post("/logout", UserController.logout);
 
 module.exports = router
