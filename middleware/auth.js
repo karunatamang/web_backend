2a96d3ed162828a5d536495706e9a457cf26763b
karunatamang <korunatamang12@gmail.com>
2020-01-28 23:03:34 -0800

    auth.js made

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,6 @@ const auth = async (req, res, next) => {
         const decoded = jwt.verify(token, 'thisismynewcourse')
         const user = await User.findOne({
             _id: decoded._id, 'tokens.token': token
-
         })
         .populate('donations').sort({ '_id': -1 });
         if (!user) {
@@ -19,5 +18,4 @@ const auth = async (req, res, next) => {
         res.status(401).send({ error: 'Please authenticate.' })
     }
 }
-
 module.exports = auth;
