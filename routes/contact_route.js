44990ddee46a723866a75384b9e10a2b6e65e5a6
karunatamang <korunatamang12@gmail.com>
2020-02-01 22:17:35 -0800

    routes contact

--- routes/contact_route.js
+++ routes/contact_route.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const auth = require('../middleware/auth');
 const router = express.Router();
-
 const ContactUsController = require("../controller/contact_controller");
 
+//routes
 router.post("/contactus", ContactUsController.addContact);
 router.get("/contactus", ContactUsController.viewContact);
 router.get("/contactus/:id", ContactUsController.viewContactById);
 router.put("/contactus/:id", ContactUsController.updateContact);
 router.delete("/contactus/:id", ContactUsController.deleteContact);
 
-module.exports = router
+module.exports = router;
